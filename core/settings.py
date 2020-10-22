b184d1ed8e731f4b34fc3cf71b2ba55af6f1c9cf
muusfa <farooq.usman@yahoo.com>
2020-10-22 16:58:38 -0600

    Setup database in Postgresql

--- core/settings.py
+++ core/settings.py
@@ -73,8 +73,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': os.path.join(BASE_DIR, 'postgresDB'),
+        'NAME': 'CapstoneDB',
+        'USER': 'standard',
+        'PASSWORD': 'CapstoneGroup3',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
