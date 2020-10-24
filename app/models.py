16eaed8428b61b246941bdfc8058a68ca42abb4d
Nafisa Tabassum <nafisa.tabassum@ucalgary.ca>
2020-10-23 23:13:35 -0600

    creates db schema

--- app/models.py
+++ app/models.py
@@ -6,6 +6,33 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
+
+class Users(models.Model):
+    pass  # By default, Django adds an id field
+
+
+class Buildings(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Floors(models.Model):
+    building_id = models.ForeignKey(Buildings, on_delete=models.PROTECT)
+    number = models.IntegerField()
+
+
+class Rooms(models.Model):
+    floor_id = models.ForeignKey(Floors, on_delete=models.PROTECT)
+    room_number = models.IntegerField()
+    max_occupancy = models.IntegerField()
+    blueprint = models.URLField()
+
+
+class Heatmaps(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
+    probabilities = ArrayField(
+        ArrayField(models.DecimalField(max_digits=11, decimal_places=10))
+    )
