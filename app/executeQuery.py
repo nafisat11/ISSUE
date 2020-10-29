42308686e1fc817f768fdfadc32a24ac24138519
muusfa <farooq.usman@yahoo.com>
2020-10-28 22:55:28 -0600

    Initial code for database to backend communication V2

--- app/executeQuery.py
+++
@@ -1,8 +0,0 @@
-#All necessary imports
-import psycopg2
-
-def executeQuery(cursor, query):
-    #Execute query    
-    cursor.execute(query)
-    print("Selecting rows from rooms table using cursor.fetchall")
-    queryResults = cursor.fetchall() 
