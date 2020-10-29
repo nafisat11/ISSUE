4f1fe7bcb9a7a86767ed7066b6d8b1ae40133bda
muusfa <farooq.usman@yahoo.com>
2020-10-28 22:55:28 -0600

    Initial code for database to backend communication V2

--- app/closeConnectionToDb.py
+++
@@ -1,8 +0,0 @@
-#All necessary imports
-import psycopg2
-
-def closeConnectionToDb(cursor, connection):
-    #Close connection to db
-    cursor.close()
-    connection.close()
-    print("PostgreSQL connection is closed")
