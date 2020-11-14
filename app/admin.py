f1bc3209ceb31db846f1776fcd59b97fc64b4c24
muusfa <farooq.usman@yahoo.com>
2020-11-14 16:57:38 -0700

    Changing methods for database and backend comms

--- app/admin.py
+++ app/admin.py
@@ -5,5 +5,10 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
+from .models import Buildings, Floors, Rooms, Heatmaps
 
 # Register your models here.
+admin.site.register(Buildings)
+admin.site.register(Floors)
+admin.site.register(Rooms)
+admin.site.register(Heatmaps)
--- app/models.py
+++ app/models.py
@@ -9,26 +9,56 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-
-
 class Users(models.Model):
+    
     pass
 
 class Buildings(models.Model):
     name = models.CharField(max_length = 200)
 
+    def get_building_name(self):
+        return self.name
+    def get_building_id(self):
+        return self.id
+
 class Floors(models.Model):
     building = models.ForeignKey(Buildings, on_delete=models.PROTECT)
     number = models.IntegerField() 
+    
+    def get_building_id(self):
+        return self.building
+    def get_floor_number(self):
+        return self.number
+    def get_floor_id(self):
+        return self.id
 
 class Rooms(models.Model):
     floor = models.ForeignKey(Floors, on_delete=models.PROTECT)
     room_number = models.IntegerField()
     max_occupancy = models.IntegerField()
+    max_pandemic_occupancy = models.IntegerField()
     blueprint = models.URLField()
+
+    def get_floor_id(self):
+        return self.floor
+    def get_room_number(self):
+        return self.room_number
+    def get_room_id(self):
+        return self.id
+    def get_max_occupancy(self):
+        return self.max_occupancy
+    def get_max_pandemic_occupancy(self):
+        return self.max_pandemic_occupancy
+    def get_blueprint(self):
+        return self.blueprint
 
 class Heatmaps(models.Model):
     user = models.ForeignKey(Users, on_delete=models.PROTECT)
     probabilities = ArrayField(
         ArrayField(models.DecimalField(max_digits=11, decimal_places=10))
     )
+    
+    def get_user(self):
+        return self.user
+    def get_probabilities(self):
+        return self.probabilities
