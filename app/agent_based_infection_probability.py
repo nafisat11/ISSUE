d4cb29988c3fe422fdde8527580ec330a74e26ce
Nafisa <nafisa.tabassum@ucalgary.ca>
2021-03-10 22:14:39 -0700

    updates to temporal graph

--- app/agent_based_infection_probability.py
+++ app/agent_based_infection_probability.py
@@ -1,11 +1,7 @@
 import math
 import json
-# import pysal as ps
-# import esda
-# from pysal.lib import weights
 
 # TODO: better structure for this
-# FIXME: need to switch input to all the seat locations otherwise it can't compute the ones where the selected agent is diagonal
 
 
 class AttackRates:
@@ -14,7 +10,8 @@ class AttackRates:
         self.mask_type = mask_type
         self.duration = int(duration)
         self.available_masks = {"N95": 0.85, "Surgical": 0.33, "Cloth": 0.11}
-        self.agents = json.loads(agents)  # all selected agents seat locations
+        # All the selected agents' seat locations
+        self.agents = json.loads(agents)
         self.normal_agents = [
             agent for agent in self.agents if agent['state'] == 1]
         self.infected_agents = [
@@ -36,7 +33,6 @@ class AttackRates:
 
     def probabilities(self):
         self.find_infected(self.agents)
-        # seat = self.find_infected(self.agents)
         for i, agent in enumerate(self.agents):
             sum_of_attackrates = 0
             if agent['state'] == 2:
@@ -45,7 +41,7 @@ class AttackRates:
                 continue
 
             for infected in self.infected:
-                # distance = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+                # Distance formula
                 dist = math.sqrt(((infected['y'] - agent['y'])**2) +
                                  ((infected['x'] - agent['x'])**2))
                 if dist <= 2:
@@ -54,11 +50,10 @@ class AttackRates:
 
             agent['attRate'] = sum_of_attackrates
             if self.mask_type in self.available_masks:
-                print(self.mask_type)
                 agent['attRate'] *= (1-self.available_masks[self.mask_type])
 
             if self.duration is not None or self.duration != 0:
-                temporal = (0.121 + 0.022*(self.duration**2))/100 + 1
+                temporal = (0.121 + 0.022*((self.duration/60)**2))/100 + 1
                 agent['attRate'] *= temporal
 
         temp_output = [[0.0, 1.000]]
@@ -66,9 +61,9 @@ class AttackRates:
         w_list = []
         att_list = []
 
-        for i in range(1, 11):
+        for i in range(5, self.duration+5, 5):
             temp_output.append(
-                [float(i), round(((0.121 + 0.022*(i**2))/100 + 1), 3)])
+                [float(i), round(((0.121 + 0.022*((i/60)**2))/100 + 1), 3)])
 
         for i, agent in enumerate(self.agents):
             innerlist = []
@@ -83,18 +78,4 @@ class AttackRates:
             att_list.append(agent['attRate'])
             output.append(innerlist)
 
-        # TODO: implementation to calculate weights without using pysal
-        # w = ps.lib.weights.DistanceBand.from_array(w_list, 2, binary=False)
-        # w = weights.DistanceBand.from_array(w_list, 2, binary=False)
-        # w.transformation = 'r'
-
-        # TODO: implementation to calculate moran's I without pysal
-        # lisa = esda.moran.Moran_Local(att_list, w)
-        # i_vals = lisa.Is
-        # p_vals = lisa.p_sim
-        # z_vals = lisa.z_sim
-
-        # print_agent_info(normal_agents)
-        # print_agent_info(infected_agents)
-        # print(i_vals)
         return output, temp_output
